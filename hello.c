b04772563d573dac3c7dc6a0c98698b68d7914f8
Devesh121 <deveshsharma1214@gmail.com>
2017-08-19 11:06:15 +0530

    next modification

--- hello.c
+++ hello.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
 int main()
-{ printf("hello world \n");
+{
+printf("enter any number \n");
+int n;
+int fac=0;
+ printf("hello world \n");
+for(int i=1;i<=n;i++)
+{
+fac=fac*i;
+}
+return fac;
+
 }
